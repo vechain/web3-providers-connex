4ac5c3329da946fb2dcdd23874bcb398635ca96f
zzGHzz <zzGHzz@github>
2022-11-06 15:44:53 +0800

    Provider wrappers for web3 & ethers

---
+++ src/compatProvider.ts
@@ -0,0 +1,75 @@
+'use strict';
+
+import { ProviderRpcError, RequestArguments } from "./eip1193";
+import { Provider } from "./provider";
+import { Net, Wallet, DelegateOpt, JsonRpcPayload, JsonRpcResponse } from './types';
+import { decodeRevertReason } from "./utils";
+
+export class ProviderWeb3 extends Provider {
+	constructor(opt: {
+		connex: Connex,
+		wallet?: Wallet,
+		net?: Net,
+		delegate?: DelegateOpt,
+	}) {
+		super(opt);
+	}
+
+	sendAsync(
+		payload: JsonRpcPayload,
+		callback: (error: any, response?: any) => void
+	) {
+		super.request(payload)
+			.then(ret => {
+				if (payload.method === 'eth_call') {
+					const errMsg = decodeRevertReason(ret);
+					if (errMsg) {
+						callback(null, {
+							id: payload.id,
+							jsonrpc: '2.0',
+							error: {
+								data: ret,
+								message: errMsg
+							}
+						})
+					}
+				} else {
+					callback(null, {
+						id: payload.id,
+						jsonrpc: '2.0',
+						result: ret
+					});
+				}
+			})
+			.catch(err => callback(err));
+	}
+}
+
+export class ProviderEthers extends Provider {
+	constructor(opt: {
+		connex: Connex,
+		wallet?: Wallet,
+		net?: Net,
+		delegate?: DelegateOpt,
+	}) {
+		super(opt);
+	}
+
+	async request(req: RequestArguments): Promise<any> {
+		try {
+			const ret = await super.request(req);
+			if (req.method === 'eth_call') {
+				const errMsg = decodeRevertReason(ret);
+				if (errMsg) {
+					return Promise.reject({
+						data: ret,
+						message: errMsg
+					})
+				}
+			}
+			return ret;
+		} catch (err: any) {
+			return Promise.reject(err as ProviderRpcError);
+		}
+	}
+}
