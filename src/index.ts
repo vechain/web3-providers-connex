bba27b6751c397624c62f70ad8ac0436b93b3ed5
zzGHzz <zzGHzz@github>
2022-11-06 15:45:24 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 'use strict';
 
-export { ConnexProvider } from './provider';
-export { Err } from './error';
+export { Provider } from './provider';
+export { ProviderWeb3, ProviderEthers } from './compatProvider';
+export * from './error';
 export * as types from './types';
 export * as utils from './utils';
 export * as ethers from './ethers';
