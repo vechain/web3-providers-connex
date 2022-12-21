e4b093b73cfa1ea9ac392a970d97ade632499d0c
Darren Kelly <darren.kelly@vechain.org>
2022-12-21 09:54:32 +0000

    using test containers for local thor

---
+++ test/testSetup.ts
@@ -0,0 +1,28 @@
+import {
+  DockerComposeEnvironment,
+  StartedDockerComposeEnvironment,
+  Wait,
+} from "testcontainers";
+import path from "path";
+
+let dockerEnvironment: StartedDockerComposeEnvironment;
+before(async function () {
+  const composePath = path.join(__dirname);
+  const composeFile = "docker-compose-test.yaml";
+
+  dockerEnvironment = await new DockerComposeEnvironment(
+    composePath,
+    composeFile
+  )
+    .withStartupTimeout(30_000)
+    .withWaitStrategy("thor-solo", Wait.forHealthCheck())
+    .up();
+
+  console.log("Docker compose environment started");
+});
+
+after(function () {
+    if (dockerEnvironment) {
+        dockerEnvironment.down();
+    }
+});
