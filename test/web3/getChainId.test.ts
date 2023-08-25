54ec5b0ac1e929aa0e9a1b95fb0a5a51669924bd
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2023-08-25 10:47:19 +0800

    update tests w.r.t. web3 v4.x

--- test/web3/getChainId.test.ts
+++ test/web3/getChainId.test.ts
@@ -2,39 +2,24 @@
 
 import 'mocha';
 import { expect, assert } from 'chai';
-import { Framework } from '@vechain/connex-framework';
-import { Driver, SimpleNet, SimpleWallet } from '@vechain/connex-driver';
-import Web3 from 'web3';
-
+import { TestObject } from '../testSetup';
+import { Web3 } from 'web3';
 import { ProviderWeb3 } from '../../src/index';
-import { urls } from '../settings';
 
-describe('Testing getChainId', () => {
-	const net = new SimpleNet(urls.mainnet);
-	const wallet = new SimpleWallet();
-
-	let driver: Driver;
-	let web3: any;
-
-	before(async () => {
-		try {
-			driver = await Driver.connect(net, wallet);
-			web3 = new Web3(new ProviderWeb3({ connex: new Framework(driver) }));
-		} catch (err: any) {
-			assert.fail('Initialization failed: ' + err);
-		}
+describe('Testing function getChainId', function () {
+	before(async function (){
+		const { eip1193Providers, connexs } = this.testObject as TestObject;
+		this.web3 = new Web3(new ProviderWeb3(eip1193Providers.main));
+		this.connex = connexs.main;
 	})
 
-	after(() => {
-		driver?.close();
-	})
 
-	it('get mainnet chainTag', async () => {
-		const tag = 74;
-		let id: number;
+	it('Should return the correct mainnet chainTag', async function () {
+		const tag = 74n;
+		let id: bigint;
 
 		try {
-			id = await web3.eth.getChainId();
+			id = await this.web3.eth.getChainId();
 		} catch (err: any) {
 			assert.fail(`Unexpected error: ${err}`);
 		}
